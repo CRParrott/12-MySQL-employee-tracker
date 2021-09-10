59d5956104fbcf1eb5ffe40c10c4fba04f41d71b
Charlie Parrott <cparrott5700@gmail.com>
2021-09-10 16:24:18 -0400

    Updating connection page

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+const util = require("util");
 const mysql = require('mysql');
 require('dotenv').config();
 
@@ -5,13 +6,12 @@ const connection = mysql.createConnection(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
 );
 
 connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
